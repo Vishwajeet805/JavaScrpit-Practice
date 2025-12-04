133b7a2fdcad72c677679d39dc5031715313158b
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-04 20:38:28 +0530

    Day 1 Added

---
+++ Day 1/index.js
@@ -0,0 +1,30 @@
+console.log("Hello, World!");
+window.alert("Welcome to my website!");
+ 
+let name="Vishwajeet";
+let age=21;
+let happy=true;
+
+document.getElementById("Myh1").textContent="Hello "+name;
+document.getElementById("myp").textContent="This is my first JavaScript code.";
+
+console.log("Name : "+name +" "+ typeof(name));
+console.log("Age : "+age +" "+ typeof(age));
+console.log("Happy : "+happy +" "+ typeof(happy));
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students 3;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;|
+
+console.log(students);
