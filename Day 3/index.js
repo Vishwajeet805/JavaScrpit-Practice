d9f689520fcde576675c79f30a63679bc6f96aac
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-04 23:39:26 +0530

    Click Counter Added

--- Day 3/index.js
+++ Day 3/index.js
@@ -5,16 +5,16 @@
 // document.getElementById("Myh1").textContent = "Hello " + username ;
 
 // Area and Circumference of Circle
-const pi=3.1459;
-let name;
+const pi=3.1445;
+let radii;
 let area;
 let circumference;
 document.getElementById("button").onclick = function() {
-    name = document.getElementById("text").value;
-    area = pi * name * name;
+    radii = document.getElementById("text").value;
+    area = pi * radii * radii;
     area=Number(area);
     document.getElementById("area").textContent = "Area is: " + area +"cm²";
-    circumference = 2 * pi * name;
+    circumference = 2 * pi * radii;
     circumference=Number(circumference);
     document.getElementById("circumference").textContent = "Circumference is: " +circumference+"cm";};
 
