e767585799d1dec63722d93a40a27cfd30aa54d5
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-06 23:04:30 +0530

    Day9 Added

---
+++ Day 9/index.js
@@ -0,0 +1,14 @@
+// callback = a function that is passed as an argument
+// to another function.
+
+// used to handle asynchronous operations:
+// 1. Reading a file
+// 2. Network requests
+// 3. Interacting with databases
+
+// "Hey, when you're done, call this next."
+// forEach() = method used to iterate over the elements
+// of an array and apply a specified function (callback)
+// to each element
+
+// array.forEach(callback)
