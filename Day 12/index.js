74c1f0b8abf123aa92629386066f0750876c6d84
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-06 23:08:58 +0530

    Day 12 Added

---
+++ Day 12/index.js
@@ -0,0 +1,38 @@
+// // object = A collection of related properties and/or methods
+// Can represent real world objects (people, products, places)
+// object = {key:value,
+// function()}
+
+// const person1 = {
+// firstName: "Spongebob",
+// lastName: "Squarepants",
+// age: 30,
+// isEmployed: true,
+// sayHello: function(){console.log("Hi! I am Spongebob!")},
+// sayBye: function(){console.log("Goodbye!")}
+
+// };
+
+// const person2 = {
+// firstName: "Patrick",
+// lastName: "Star",
+// age: 42,
+// isEmployed: false,
+// sayHello: () => console.log("Hey I'm Patrick ... "),
+// sayBye: function(){console.log("Bye ... ")}
+
+// console.log(person1.firstName);
+// console.log(person1.lastName);
+// console.log(person1.age);
+// console.log(person1.isEmployed);
+
+// // this = reference to the object where THIS is used
+// (the object depends on the immediate context)
+// person.name = this.name
+
+// const person1 = {
+// name: "Spongebob",
+// favFood: "hamburgers",
+// sayHello: function(){console.log('Hi! I am ${this.name} )}
+
+// person1.sayHello();
