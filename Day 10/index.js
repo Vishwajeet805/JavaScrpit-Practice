aa30fd15f1b10c7f23b8db18a1b7d40b7b71ee60
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-06 23:06:22 +0530

    Day 10 Added

---
+++ Day 10/index.js
@@ -0,0 +1,33 @@
+// .map() = accepts a callback and applies that function
+// to each element of an array, then return a new array
+
+// // .filter() = creates a new array by filtering
+// out elements
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// let evenNums = numbers.filter(isEven);
+// let oddNums = numbers.filter(isOdd);
+
+// console. log(oddNums
+
+// function isEven(element){
+// return element % 2 === 0;
+
+// }
+
+// function isOdd(element){
+// return element % 2 !== 0;
+
+// }
+
+// .reduce() = reduce the elements of an array
+// to a single value
+
+// const prices = [5, 30, 10, 25, 15, 20];
+
+// const total = prices.reduce(sum);
+
+// console.log($${total.toFixed(2)});
+
+// function sum(accumulator, element){
+// return accumulator + element;
