19177eaab026f0cbbfce5f90e460e1b80a0fb03c
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-08 03:29:32 +0530

    Day 17 Added

---
+++ Day 17/index.js
@@ -0,0 +1,32 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDay();
+console.log(date)
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+console. log(dayOfWeek);
+
+date.setFullYear(2024);
+date.setMonth(5);
+date.setDate(5);
+date.setHours(4);
+date.setMinutes(45);
+date.setSeconds(45);
+date.setDayOfWeek(4);
+
+console.log(Year);
+console.log(Month);
+console.log(Day);
+console.log(Hour);
+console.log(Minutes);
+console.log(Seconds);
+console. log(DayOfWeek);
