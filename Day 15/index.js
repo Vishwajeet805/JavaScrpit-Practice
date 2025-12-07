c2652872cd114b883b44f0e0d13c947ba869a862
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-08 03:04:47 +0530

    Day 15 Added

--- Day 15/index.js
+++ Day 15/index.js
@@ -28,3 +28,34 @@ console.log(person.address.country);
 for(const property in person.address){
         console.log(person.address[property]);
 }
+
+
+class Person{
+        constructor(namel,age,...address){
+                this.namel=namel;
+                this.age=age;
+                this.address=new address(...address)
+        };
+
+}
+class Address{
+        constructor(street,city,state){
+                this.street=street;
+                this.city=city;
+                this.state=state;
+        }
+}
+const person1=new Person("han",12,"21","AB","Mp");
+
+console.log(person1.address.state);
+
+
+const fruits=[{n:"A",a:1,F:"11"},
+              {n:"B",a:2,F:"22"},
+              {n:"C",a:3,F:"33"},
+              {n:"D",a:4,F:"44"},
+              {n:"E",a:5,F:"55"}
+]
+
+console.log(fruits[2].F);
+fruits.forEach(fruit=>console.log(fruit))
